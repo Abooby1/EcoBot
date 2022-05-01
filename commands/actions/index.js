4642baf9df7b06d024df804b6183388377c6b515
Abicam Studios <97899737+Abooby1@users.noreply.github.com>
2022-05-01 23:36:56 +0000

    updates

--- commands/actions/index.js
+++ commands/actions/index.js
@@ -1,8 +1,10 @@
 import {Test, TempBan, SetMoney, ResetData} from "./debug.js"
+import {Help} from "./help.js"
 
 export const Actions = [
   Test,
   TempBan,
   SetMoney,
-  ResetData
+  ResetData,
+  Help
 ]
